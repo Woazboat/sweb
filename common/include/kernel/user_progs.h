779275b3aa741f77c6b660063cd7c9fe695bbcf3
Florian Kargl <florian.kargl@student.tugraz.at>
2018-03-04 02:40:31 +0100

    Don't append .sweb to userspace binaries

--- common/include/kernel/user_progs.h
+++ common/include/kernel/user_progs.h
@@ -3,7 +3,7 @@
 // DO NOT CHANGE THE NAME OR THE TYPE OF THE user_progs VARIABLE!
 char const *user_progs[] = {
 // for reasons of automated testing
-                            "/usr/shell.sweb",
+                            "/usr/shell",
                             0
                            };
 
